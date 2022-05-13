1f47ed525bbebb6eeef16889bbf3ccba4e1ddc47
sergivi11 <sergivi04@gmail.com>
2022-05-13 17:12:07 +0200

    Fix xut and sounds

--- Assets/Scripts/PlayerBallMechanism.cs
+++ Assets/Scripts/PlayerBallMechanism.cs
@@ -25,39 +25,42 @@ public class PlayerBallMechanism : MonoBehaviour
     void Update()
     {
         //poner condicion de hacer solo si tienes a la pelota como hija
-            if (Input.GetButtonDown("Fire1") && ball.transform.parent != null)
-            {
-                StartCoroutine("Shoot");
-            }
+        if (Input.GetButtonDown("Fire1") && ball.transform.parent != null)
+        {
+            GetComponent<AudioSource>().Play();
+            StartCoroutine("Shoot");
+        }
     }
 
     private void OnTriggerEnter(Collider other)
     {
         //Coger Pelotiña
-        if(other.tag == "Ball" && !shooting)
+        if (other.tag == "Ball" && !shooting)
         {
-            if(ball == null) ball = other.gameObject; 
+            if (ball == null) ball = other.gameObject;
             ball.GetComponent<Rigidbody>().velocity = Vector3.zero;
             ball.GetComponent<Rigidbody>().angularVelocity = Vector3.zero;
             ball.transform.GetChild(0).GetComponent<SphereCollider>().enabled = false;
             ball.transform.position = new Vector3(0, 0, 0);
             ball.transform.SetParent(this.transform);
             ball.transform.position = ballPlayerPosition.transform.position;
-            
+
         }
     }
 
     private IEnumerator Shoot()
     {
+
         shooting = true;
         ball.transform.SetParent(null);
         ball.transform.GetChild(0).GetComponent<SphereCollider>().enabled = true;
-        ball.GetComponent<Rigidbody>().AddForce(transform.TransformDirection(Vector3.forward) * speed*10);
+        ball.GetComponent<Rigidbody>().AddForce(transform.TransformDirection(Vector3.forward) * speed * 10);
         //Decimos que la pelota, para que no tenga inercia de mas se pare en 2 segundos. 
         yield return new WaitForSeconds(stopBallTimer);
         ball.transform.GetComponent<SphereCollider>().enabled = true;
-        ball.GetComponent<Rigidbody>().velocity= Vector3.zero;
+        ball.GetComponent<Rigidbody>().velocity = Vector3.zero;
         ball.GetComponent<Rigidbody>().angularVelocity = Vector3.zero;
+
         shooting = false;
         StopCoroutine("Shoot");
     }
